854ccb1afa9ff3002f4451f319feee465f2414e4
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-11 15:12:31 +0100

    Fixed typos and now including data files in setup.

--- unimi_unsubscribe/unimi_unsubscribe.py
+++ unimi_unsubscribe/unimi_unsubscribe.py
@@ -7,6 +7,14 @@ from time import sleep
 
 
 def unsubscribe(user: str, password: str, server: str="smtp.unimi.it", sympa_mail: str="sympa@liste.unimi.it", port: int=465, timeout: int= 5):
+    """Method to unsubscribe from given sympa service.
+        user: str, Your user name in the server, usually follows a format such as name.surname@studenti.unimi.it
+        password: str, Your account password. We use SSL.
+        server: str="smtp.unimi.it", Your SSL SMTP server account
+        sympa_mail: str="sympa@liste.unimi.it", The address to which to send the unsubscribe mails.
+        port: int=465, The SSL server port
+        timeout: int= 5, timeout to be waited beetween mails.
+    """
     server_ssl = SMTP_SSL(server, port)
     server_ssl.login(user, password)
     spam_path = "{dir}/spam_list.json".format(
@@ -18,9 +26,11 @@ def unsubscribe(user: str, password: str, server: str="smtp.unimi.it", sympa_mai
     pprint(spam_lists)
     print(
         "To avoid being flagged as spam, a timeout of {timeout} seconds will be waited beetween each mail.".format(timeout=timeout))
+    print("If you'd like to customize said timeout, you can pass an optional parameter `timeout` when calling `unsubscribe`.")
     sure = input("Proceed? [y/n]")
     if sure != "y":
         print("Entered {sure}, aborting.".format(sure=sure))
+        return
     for spam_list in tqdm(spam_lists):
         server_ssl.sendmail(user, sympa_mail, spam_list)
         sleep(timeout)
